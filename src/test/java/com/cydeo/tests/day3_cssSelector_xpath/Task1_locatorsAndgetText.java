7d6b9a50c3a8698aad2637823af87932d8ad4b87
recep <recepdemir1204@gmail.com>
2022-12-28 12:19:53 +0300

    selenium lesson task solutions

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/Task1_locatorsAndgetText.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/Task1_locatorsAndgetText.java
@@ -1,16 +1,16 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task1_locatorsAndgetText {
     public static void main(String[] args) {
         //1- Open a chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver= new ChromeDriver();
+        //WebDriverManager.chromedriver().setup();
+        //WebDriver driver= new ChromeDriver();
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
 
         //2- Go to: https://login1.nextbasecrm.com/
@@ -22,8 +22,23 @@ public class Task1_locatorsAndgetText {
         WebElement inputUserName= driver.findElement(By.className("login-inp"));
         inputUserName.sendKeys("incorrect");
 
+        //4- Enter incorrect password: “incorrect”
+        WebElement inputPassword= driver.findElement(By.name("USER_PASSWORD"));
+        inputPassword.sendKeys("incorrect");
 
+        //5- Click to login button.
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
 
+        //6- Verify error message text is as expected:..Expected: Incorrect login or password
+        WebElement errrorMessage = driver.findElement(By.className("errortext"));
+        String expectedErrorMessage= "Incorrect login or password";
+        String actualErrorMessage= errrorMessage.getText();
+        if(actualErrorMessage.equals(expectedErrorMessage)){
+            System.out.println("PASSED!");
+        }else{
+            System.out.println("FAİLED!");
+        }
 
     }
 }
