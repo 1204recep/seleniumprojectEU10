5b2b85db4acb85d43a9c4547e9edcacbc135f748
recep <recepdemir1204@gmail.com>
2022-12-31 10:46:10 +0300

    selenium lesson task solutions

---
+++ src/test/java/com/cydeo/tests/day4_findElement_checkboxes_radio/Task6_StaleElementReferenceException.java
@@ -0,0 +1,36 @@
+package com.cydeo.tests.day4_findElement_checkboxes_radio;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task6_StaleElementReferenceException {
+    public static void main(String[] args) {
+        //TC #6: StaleElementReferenceException Task
+        //1- Open a chrome browser
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2- Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
+
+        //3- Locate “CYDEO” link, verify it is displayed.
+        WebElement cydeoLink = driver.findElement(By.xpath("//a[.='CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
+
+        //4- Refresh the page.
+        driver.navigate().refresh();
+
+                // a.We are refreshing the web element reference by re_assigning(re_locating) the web element
+                // b.driver refresh yapınca cydeolink ile bağlantısını kaybetti..bağlantıyı tekrar tesisi etmek için aynı
+                // attribute  (cydeolink) yeniden locate tanımlaması yaptık
+        cydeoLink = driver.findElement(By.xpath("//a[.='CYDEO']"));
+
+        //5- Verify it is displayed, again.
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
+    }
+}
